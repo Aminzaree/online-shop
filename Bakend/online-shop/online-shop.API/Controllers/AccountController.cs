143071566ab83d3ca00815089cfd4e5535a03c5a
Nader <Nader@DESKTOP-99PEP3D>
2024-10-01 22:10:24 +0330

    Initial Project Backend

---
+++ Bakend/online-shop/online-shop.API/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace online_shop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult Login()
+        {
+            return Ok("تست");
+        }
+    }
+}
