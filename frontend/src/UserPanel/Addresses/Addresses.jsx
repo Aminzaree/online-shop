fa4ea4318fb94262da7264482f89974157ec9e6b
Amin <124307503+Aminzaree@users.noreply.github.com>
2024-11-17 22:00:36 +0330

    Update Addresses View

--- frontend/src/UserPanel/Addresses/Addresses.jsx
+++ frontend/src/UserPanel/Addresses/Addresses.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button, image, user } from "@nextui-org/react";
 import { Dropdown, Link, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { CiEdit } from "react-icons/ci";
 import { IoAppsOutline } from "react-icons/io5";
@@ -8,8 +8,41 @@ import { AiOutlineUser } from "react-icons/ai";
 import { SlLocationPin } from "react-icons/sl";
 
 export default function Addresses() {
+
+    const addressesObject = [
+        {
+            id: 1,
+            area: "سعادت آباد - خیابان صراف‌ها - پلاک ۱۲ واحد ۴",
+            city: "تهران",
+            zipCode: "۱۶۸۵۶۵۶۶۸۵",
+            phoneNumber: "۰۹۳۶۳۰۶۳۶۷",
+            userName: "karbar_froshgah",
+            image: imageMap
+        },
+        {
+            id: 2,
+            area: "سعادت آباد - خیابان صراف‌ها - پلاک ۱۲ واحد ۴",
+            city: "تهران",
+            zipCode: "۱۶۸۵۶۵۶۶۸۵",
+            phoneNumber: "۰۹۳۶۳۰۶۳۶۷",
+            userName: "karbar_froshgah",
+            image: imageMap
+        },
+        {
+            id: 3,
+            area: "سعادت آباد - خیابان صراف‌ها - پلاک ۱۲ واحد ۴",
+            city: "تهران",
+            zipCode: "۱۶۸۵۶۵۶۶۸۵",
+            phoneNumber: "۰۹۳۶۳۰۶۳۶۷",
+            userName: "karbar_froshgah",
+            image: ""
+        },
+
+    ];
+
     return (
         <div className="w-full">
+            {/* Add New Address */}
             <div className="w-full text-end">
                 <Button
                     color="danger"
@@ -19,99 +52,65 @@ export default function Addresses() {
                     }
                 >ثبت آدرس جدید</Button>
             </div>
-            <div className="w-full mt-8 pb-4 border-b">
-                <div className="w-full flex items-center justify-between">
-                    <p> سعادت آباد - خیابان صراف‌ها - پلاک ۱۲ واحد ۴</p>
-                    <Dropdown backdrop="blur">
-                        <DropdownTrigger>
-                            <Button
-                                variant="flat"
-                                color="warning"
-                                isIconOnly
-                            >
-                                <IoAppsOutline />
-                            </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu variant="faded" aria-label="Static Actions">
-                            <DropdownItem
-                                key="new"
-                                startContent={
-                                    <CiEdit size={20} />
-                                }
-                            >ویرایش آدرس</DropdownItem>
-                            <DropdownItem
-                                key="delete"
-                                className="text-danger"
-                                color="danger"
-                                startContent={
-                                    <MdDelete size={20} />
-                                }
-                            >
-                                حذف
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </div>
-                <div className="w-full flex items-center justify-between mt-4">
-                    <div>
-                        <p className="flex items-center text-zinc-500 "><SlLocationPin className="me-1 " />  تهران</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><MdAlternateEmail className="me-1" /> ۱۶۸۵۶۵۶۶۸۵</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><MdOutlineLocalPhone className="me-1" /> ۰۹۳۶۳۰۶۳۶۷</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><AiOutlineUser className="me-1" /> karbar froshgah</p>
-                    </div>
-                    <img
-                        src={imageMap}
-                        alt="عکس نقشه"
-                        className="rounded-lg w-36"
-                    />
-                </div>
-            </div>
+
             <div className="w-full mt-8 pb-4">
-                <div className="w-full flex items-center justify-between">
-                    <p> سعادت آباد - خیابان صراف‌ها - پلاک ۱۲ واحد ۴</p>
-                    <Dropdown backdrop="blur">
-                        <DropdownTrigger>
-                            <Button
-                                variant="flat"
-                                color="warning"
-                                isIconOnly
-                            >
-                                <IoAppsOutline />
-                            </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu variant="faded" aria-label="Static Actions">
-                            <DropdownItem
-                                key="new"
-                                startContent={
-                                    <CiEdit size={20} />
-                                }
-                            >ویرایش آدرس</DropdownItem>
-                            <DropdownItem
-                                key="delete"
-                                className="text-danger"
-                                color="danger"
-                                startContent={
-                                    <MdDelete size={20} />
-                                }
-                            >
-                                حذف
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </div>
-                <div className="w-full flex items-center justify-between mt-4">
-                    <div>
-                        <p className="flex items-center text-zinc-500 "><SlLocationPin className="me-1 " />  تهران</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><MdAlternateEmail className="me-1" /> ۱۶۸۵۶۵۶۶۸۵</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><MdOutlineLocalPhone className="me-1" /> ۰۹۳۶۳۰۶۳۶۷</p>
-                        <p className="flex items-center text-zinc-500 mt-2"><AiOutlineUser className="me-1" /> karbar froshgah</p>
-                    </div>
-                    <img
-                        src={imageMap}
-                        alt="عکس نقشه"
-                        className="rounded-lg w-36"
-                    />
-                </div>
+                {
+                    addressesObject.map((item, index, array) => {
+
+                        const { id, area, city, zipCode, phoneNumber, userName, image } = item;
+
+                        return (
+                            <div className={`py-4 border-b ${index === array.length - 1 ? "border-b-0" : ""}`} key={id}>
+                                <div className="w-full flex items-center justify-between">
+                                    <p>{area}</p>
+                                    <Dropdown backdrop="blur">
+                                        <DropdownTrigger>
+                                            <Button
+                                                variant="flat"
+                                                color="warning"
+                                                isIconOnly
+                                            >
+                                                <IoAppsOutline />
+                                            </Button>
+                                        </DropdownTrigger>
+                                        <DropdownMenu variant="faded" aria-label="Static Actions">
+                                            <DropdownItem
+                                                key="new"
+                                                startContent={
+                                                    <CiEdit size={20} />
+                                                }
+                                            >ویرایش آدرس</DropdownItem>
+                                            <DropdownItem
+                                                key="delete"
+                                                className="text-danger"
+                                                color="danger"
+                                                startContent={
+                                                    <MdDelete size={20} />
+                                                }
+                                            >
+                                                حذف
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </Dropdown>
+                                </div>
+                                <div className="w-full flex items-center justify-between mt-4">
+                                    <div>
+                                        <p className="flex items-center text-zinc-500 "><SlLocationPin className="me-1 " /> {city}</p>
+                                        <p className="flex items-center text-zinc-500 mt-2"><MdAlternateEmail className="me-1" /> {zipCode}</p>
+                                        <p className="flex items-center text-zinc-500 mt-2"><MdOutlineLocalPhone className="me-1" /> {phoneNumber}</p>
+                                        <p className="flex items-center text-zinc-500 mt-2"><AiOutlineUser className="me-1" /> {userName}</p>
+                                    </div>
+                                    <img
+                                        src={image ? image : imageMap}
+                                        alt="عکس نقشه"
+                                        className="rounded-lg w-36"
+                                    />
+                                </div>
+                            </div>
+                        )
+                        
+                    })
+                }
             </div>
         </div>
     );
