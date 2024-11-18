27762893d8975f36a445dcc1d4e646136885d444
Amin <124307503+Aminzaree@users.noreply.github.com>
2024-11-18 21:45:44 +0330

    Update Favorite View

--- frontend/src/UserPanel/Favorite/Favorite.jsx
+++ frontend/src/UserPanel/Favorite/Favorite.jsx
@@ -3,41 +3,84 @@ import { MdDelete } from "react-icons/md";
 import { TbBuildingWarehouse, TbShoppingCart } from "react-icons/tb";
 
 export default function Favorite() {
+
+    const favoriteList = [
+        {
+            id: 1,
+            label: "گوشی موبایل iphone 16",
+            inventory: true,
+            price: "۴۰,۱۹۹,۰۰۰",
+            image: "https://nextui.org/images/hero-card-complete.jpeg"
+        },
+        {
+            id: 2,
+            label: "گوشی موبایل iphone 16 pro",
+            inventory: false,
+            price: "۴۰,۱۹۹,۰۰۰",
+            image: "https://nextui.org/images/hero-card-complete.jpeg"
+        },
+        {
+            id: 3,
+            label: "گوشی موبایل iphone 16 pro",
+            inventory: false,
+            price: "۴۰,۱۹۹,۰۰۰",
+            image: "https://nextui.org/images/hero-card-complete.jpeg"
+        },
+    ]
+
+
     return (
         <div className="w-full">
             <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Card className="w-full py-4">
-                    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                        <Image
-                            alt="Card background"
-                            className="object-cover rounded-xl"
-                            src="https://nextui.org/images/hero-card-complete.jpeg"
-                        />
-                    </CardHeader>
-                    <CardBody className="overflow-visible text-start py-2 px-4">
-                        <p className="pt-1">گوشی موبایل iphone 16</p>
-                        <small className="flex items-center text-default-500 pt-2"><TbBuildingWarehouse className="me-1" /> موجود در انبار</small>
-                        <h4 className="font-bold text-large text-end">۴۰,۱۹۹,۰۰۰ تومان</h4>
-                    </CardBody>
-                    <CardFooter>
-                        <Button
-                            color="danger"
-                            variant="flat"
-                            isIconOnly
-                            className="w-1/6 me-2"
-                        >
-                            <MdDelete size={20} />
-                        </Button>
-                        <Button 
-                            color="success" 
-                            variant="ghost"
-                            className="w-5/6"
-                        >
-                            <TbShoppingCart size={20} />
-                            افزودن به سبد
-                        </Button>
-                    </CardFooter>
-                </Card>
+
+                {
+                    favoriteList.map((item, index) => {
+
+                        const { id, label, inventory, price, image } = item;
+
+                        return (
+                            <Card
+                                key={id}
+                                isHoverable
+                                className="w-full py-4"
+                            >
+                                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                                    <Image
+                                        alt="Card background"
+                                        className="object-cover rounded-xl"
+                                        src={image}
+                                    />
+                                </CardHeader>
+                                <CardBody className="overflow-visible text-start py-2 px-4">
+                                    <p className="pt-1">{label}</p>
+                                    <small className={`flex items-center text-default-500 pt-2 ${inventory ? "text-default-400" : "text-danger-600"}`}><TbBuildingWarehouse className="me-1" />{inventory ? "موجود در انبار" : "ناموجود در انبار"}</small>
+                                    <h4 className="font-bold text-large text-end">{price} تومان</h4>
+                                </CardBody>
+                                <CardFooter>
+                                    <Button
+                                        color="danger"
+                                        variant="flat"
+                                        isIconOnly
+                                        className="w-1/6 me-2"
+                                    >
+                                        <MdDelete size={20} />
+                                    </Button>
+                                    <Button
+                                        color="success"
+                                        variant="ghost"
+                                        className="w-5/6"
+                                    >
+                                        <TbShoppingCart size={20} />
+                                        افزودن به سبد
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        )
+
+                    })
+                }
+
+
             </div>
         </div>
     );
